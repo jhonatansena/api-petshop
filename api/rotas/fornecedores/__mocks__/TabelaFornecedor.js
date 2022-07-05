d3530368ec575109d7156aef5dfc9b0267d72ede
Jhonatan Sena <jcostasenna@hotmail.com>
2022-07-05 11:21:41 -0300

    test: add fornecedor mock

---
+++ api/rotas/fornecedores/__mocks__/TabelaFornecedor.js
@@ -0,0 +1,30 @@
+const Modelo = require('./ModeloTabelaFornecedor')
+const NaoEncontrado = require('../../erros/NaoEncontrado')
+
+module.exports = {
+  listar () {
+    return []
+  },
+
+  inserir (fornecedor) {
+    return { 
+        id: 500,
+        dataCriacao: '10/12/2035',
+        dataAtualizacao: '10/12/2035',
+        versao: 90
+    }
+  },
+
+  async pegarPorId (id) {
+    return { 
+        id: 500,
+        dataCriacao: '10/12/2035',
+        dataAtualizacao: '10/12/2035',
+        versao: 90
+    }
+  },
+
+  async atualizar (id, dadosParaAtualizar) {},
+
+  async remover (id) {}
+}
